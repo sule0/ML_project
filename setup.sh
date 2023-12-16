017a48daca491dcbc33c6a5ddf93fef920e99434
Şule Nur Çakır <99013587+sule0@users.noreply.github.com>
2023-12-16 03:19:51 +0300

    heroku

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/.streamlit/
+
+echo "[general]
+email = \"your-email@domain.com\"
+" > ~/.streamlit/credentials.toml
+
+echo "[server]
+headless = true
+enableCORS=false
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
